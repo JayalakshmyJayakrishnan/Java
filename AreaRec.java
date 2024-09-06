7eabb573eaea388f376ac9b461ba143be5581f6d
Mathew Joseph t a <144910668+mathewjosephta@users.noreply.github.com>
2024-09-06 20:04:41 +0530

    Update AreaRec.java
    1. Removed undeclared 'area' variable in the 'getArea()' method:
                    Instead of using a separate variable, you can directly return the result of length * breadth.
    
    2. Changed setdimension to setDimension:
                    Following Java naming conventions (camelCase) for method names, the method was renamed to setDimension.
    
    3.Consistent data types:
                  In the original code, you used float for length and breadth, so I ensured that getArea() returns float as well, for consistency.
    
    4. Added sc.close():
                  It's a good practice to close the Scanner object after use to avoid resource leaks.

--- AreaRec.java
+++ AreaRec.java
@@ -1,37 +1,38 @@
 import java.util.Scanner;
 
+class Area {
+    private float length;
+    private float breadth;
+
+    // Corrected method name to follow camelCase convention
+    public void setDimension(float length, float breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public float getArea() {
+        // No need to declare a new variable, return the result directly
+        return length * breadth;
+    }
+}
+
 public class AreaRec {
     public static void main(String[] args) {
         Area rect = new Area();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("ENTER THE DIMENSIONS \n");
-        System.out.println("Enter the Length:");
-        Float length =sc.nextFloat();
+        System.out.print("Enter the Length: ");
+        float length = sc.nextFloat();
        
         System.out.print("Enter the Breadth: ");
-        Float breadth =sc.nextFloat();
+        float breadth = sc.nextFloat();
 
+        rect.setDimension(length, breadth);
 
-        rect.setdimension(length, breadth);
-
-        Float area = rect.getArea();
-        System.out.print("Area of the Rectangle: " + area);
-        }
-    
+        float area = rect.getArea();
+        System.out.println("Area of the Rectangle: " + area);
+        sc.close();
+    }
 }
 
-class Area {
-    float length;
-    float breadth;
-  
-
-    public void setdimension (float length, float breadth) {
-        this.length = length;
-        this.breadth = breadth;
-    }
-    public double getArea (){
-        area = length*breadth;
-        return area;
-    }
-} 
